7f35759ecca6bbfa8ca811011a2d5a139336b07f
Diana Petre <petre.diana98@yahoo.ro>
2023-07-12 18:31:33 +0300

    feat: Add logger provider

---
+++ src/provider/logger/consoleLoggerProvider.ts
@@ -0,0 +1,7 @@
+import {LoggerProvider} from "./contracts";
+
+export class ConsoleLoggerProvider implements LoggerProvider {
+    public log(message: unknown): void {
+        console.log(message);
+    }
+}

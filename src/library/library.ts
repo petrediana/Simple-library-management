aa0be134f015696b177796036116f007c3222432
Diana Petre <petre.diana98@yahoo.ro>
2023-07-12 18:36:19 +0300

    ref: Library manager for a better extendability

--- src/library/library.ts
+++ src/library/library.ts
@@ -1,84 +1,86 @@
-import {Book} from "../book/book";
-import {Borrowed} from "../book/borrowed";
-import {TimeZone} from "../service/time/timeZone";
-import {Time} from "../service/time/contracts";
+import {Book} from "../book";
+import {Borrowed} from "../book";
+import {DefaultDateTimeProvider} from "../provider/time";
+import {DateTimeProvider} from "../provider/time/contracts";
+import {BookStore} from "../contracts";
+import {LoggerProvider} from "../provider/logger/contracts";
+import {PenaltyProvider} from "../provider/penalty/contracts";
+import {ConsoleLoggerProvider} from "../provider/logger";
+import {DefaultPenaltyCalculator} from "../provider/penalty";
 
-export class Library {
-    private readonly books: Book[];
-    private readonly availableBooks: Book[];
+export class Library implements BookStore {
+    private readonly store: BookStore;
     private readonly borrowedBooks: Borrowed[];
 
-    private readonly time: Time;
+    private readonly dateTimeProvider: DateTimeProvider;
+    private readonly loggerProvider: LoggerProvider;
+    private readonly penaltyProvider: PenaltyProvider;
 
-    public constructor(books: Book[], time: TimeZone = new TimeZone()) {
-        this.books = [...books];
-        this.availableBooks = [...books];
+    public constructor(store: BookStore, dateTimeProvider: DateTimeProvider = new DefaultDateTimeProvider(),
+                       loggerProvider: LoggerProvider = new ConsoleLoggerProvider(),
+                       penaltyProvider: PenaltyProvider = new DefaultPenaltyCalculator(dateTimeProvider)) {
+        this.store = store;
         this.borrowedBooks = [];
-        this.time = time;
+        this.dateTimeProvider = dateTimeProvider;
+        this.loggerProvider = loggerProvider;
+        this.penaltyProvider = penaltyProvider;
     }
 
     public add(book: Book): void {
-        this.books.push(book);
-        this.availableBooks.push(book);
+        this.store.add(book);
     }
 
-    public canBeBorrowed(book: Book): boolean {
-        const isAvailable = this.availableBooks.find(available => available.name === book.name);
-        return !!isAvailable;
+    public isAvailable(book: Book): boolean {
+        return this.store.isAvailable(book);
     }
 
-    public toBorrow(book: Book): Borrowed {
-        if (this.canBeBorrowed(book)) {
-            const index = this.availableBooks.findIndex(available => available.name === book.name);
-            this.availableBooks.splice(index, 1);
-
-            const borrowed = new Borrowed(book, this.time.getCurrent());
-            this.borrowedBooks.push(borrowed);
-
-            return borrowed;
-        }
-        throw new Error("Book can not be borrowed...");
+    public isFromThisStore(book: Book): boolean {
+        return this.store.isFromThisStore(book);
     }
 
-    public toReturn(book: Book): void {
-        const isBookFromThisLibrary = this.books.find(thisBook => thisBook.name === book.name);
+    public borrow(book: Book, startingFrom: Date = this.dateTimeProvider.getCurrentDateTime()): Borrowed {
+        const borrowed = this.store.borrow(book, startingFrom);
+        this.borrowedBooks.push(borrowed);
 
-        if (!isBookFromThisLibrary) {
-            throw new Error("The book that you are trying to return does not belong to this Library!");
-        }
+        return borrowed;
+    }
 
-        const index = this.borrowedBooks.findIndex(borrowed => borrowed.book.name === book.name);
-        if (index === -1) {
+    public returnBack(book: Book): void {
+        const borrowed = this.borrowedBooks.find((borrowed) => borrowed.book.name === book.name);
+        if (!borrowed) {
             throw new Error("This book was not borrowed from this library!");
         }
-
-        const borrowed = this.borrowedBooks[index];
-        const borrowedDate = new Date(borrowed.borrowedDate);
-        const returnDate = new Date(this.time.getCurrent());
-        const differenceInDays = (returnDate.getTime() - borrowedDate.getTime()) / (1000 * 3600 * 24);
-
-        if (differenceInDays >= 15) {
-            console.log("You are late with the book and need to pay a penalty...");
-        } else {
-            console.log("Book returned with no penalties.")
+        
+        const penalty = this.penaltyProvider.calculate(borrowed);
+        if (penalty !== 0) {
+            this.loggerProvider.log(`You have to pay this penalty: **${penalty}**!`)
         }
 
-        this.availableBooks.push(book);
-        this.borrowedBooks.splice(index, 1);
+        this.store.returnBack(book);
+        this.borrowedBooks.splice(this.borrowedBooks.indexOf(borrowed), 1);
     }
 
+    public getAll(): Book[] {
+        return [...this.store.getAll()];
+    }
+
+    public getAvailable(): Book[] {
+        return [...this.store.getAvailable()];
+    }
+
+    // TODO: maybe move the displays to dedicated class...
     public displayAllBooks(): void {
-        console.log("These are all the books in the library...");
-        console.log(this.books);
+        this.loggerProvider.log("These are all the books in the library...");
+        this.loggerProvider.log(this.store.getAll());
     }
 
     public displayAvailableBooks(): void {
-        console.log("These are all the books available to be borrowed in the library...");
-        console.log(this.availableBooks);
+        this.loggerProvider.log("These are all the books available to be borrowed in the library...");
+        this.loggerProvider.log(this.store.getAvailable());
     }
 
     public displayBorrowedBooks(): void {
-        console.log("These are all the borrowed books from the library...");
-        console.log(this.borrowedBooks);
+        this.loggerProvider.log("These are all the borrowed books from the library...");
+        this.loggerProvider.log(this.borrowedBooks);
     }
 }
